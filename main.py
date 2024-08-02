482c0fead67734891fbc6056c7849e9330186ee4
KamilloDev <krystiankowalczyk1204@gmail.com>
2024-08-02 13:56:06 +0200

    Added a tilemap, animation, editor

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame, sys
-from data.util.utils import load_image, load_images
-from data.scripts.player import Player
+from data.scripts.utils import load_image, load_images, Animation
+from data.scripts.entity import Player
 from data.scripts.tilemap import Tilemap
+
 
 class Game:
     def __init__(self):
@@ -9,20 +10,30 @@ class Game:
         self.screen = pygame.display.set_mode((800, 560))
         self.display = pygame.Surface((400, 280))
         self.clock = pygame.time.Clock()
-        
-        self.keys = {'left': False,
-                     'right': False}
-        
+    
+       
         """'players': load_images('data/images')"""
         
-        #'data/images/'
+        # def path = 'data/images/'
         self.assets = {'background': load_image('Background.png'),
                        'icon': load_image('player_idle_0.png'),
-                       'grass': load_image('tiles/grass/grass_0.png')}
+                       'grass': load_images('tiles/grass'),
+                       'decor': load_images('tiles/decor'),
+                       'large_decor': load_images('tiles/large_decor'),
+                       'stone': load_images('tiles/stone'),
+                       'player': load_image('player_idle_0.png'),
+                       'player/idle': Animation(load_images('entities/player'))}
+        
         
         self.tilemap = Tilemap(self, tile_size=16)
+        self.tilemap.load('Map.json')
         
-        self.player = Player([50, 50])
+        self.player = Player(self, [200, 50], [19, 27])
+        self.keys = {'left': False, 'right': False}
+        
+        self.movement = [False, False]
+        
+        self.scroll = [0, 0]
         
         pygame.display.set_icon(self.assets['icon'])
     
@@ -34,23 +45,28 @@ class Game:
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_a:
-                        self.keys['left'] = True
+                        self.movement[0] = True
                     if event.key == pygame.K_d:
-                        self.keys['right'] = True
+                        self.movement[1] = True
                     if event.key == pygame.K_SPACE:
                         self.player.jump()
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_a:
-                        self.keys['left'] = False
+                        self.movement[0] = False
                     if event.key == pygame.K_d:
-                        self.keys['right'] = False
+                        self.movement[1] = False
             
             pygame.display.set_caption(f'TechShoot | Fps: {str(int(self.clock.get_fps()))}')
             
-            self.display.blit(self.assets['background'], (0, 0))
-            self.tilemap.render(self.display)
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+            self.render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
             
-            self.player.apply_gravity(self.keys)
+            
+            self.display.blit(self.assets['background'], (0, 0))
+            self.tilemap.render(self.display, offset=self.render_scroll)
+            
+            self.player.update(self.tilemap, self.movement)
             self.player.render(self.display)
             
             
